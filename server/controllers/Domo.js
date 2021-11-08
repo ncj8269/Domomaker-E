5d0edd4d0c60ca1b2b57a33d0a54bd9df20bc1bd
Nicolas Jensen <ncj8269@ad.rit.edu>
2021-11-07 21:33:06 -0500

    added height

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -14,13 +14,14 @@ const makerPage = (req, res) => {
 };
 
 const makeDomo = (req, res) => {
-  if (!req.body.name || !req.body.age) {
-    return res.status(400).json({ error: 'RAWR! Both name and age are required' });
+  if (!req.body.name || !req.body.age || !req.body.height) {
+    return res.status(400).json({ error: 'RAWR! Name age and height are required' });
   }
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
+    height: req.body.height,
     owner: req.session.account._id,
   };
 
